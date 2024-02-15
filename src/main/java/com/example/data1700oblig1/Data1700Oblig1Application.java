299fa4cea57bee274ac9dffeb068f46e312ab4f4
Siggendiggen <sigridaar@gmail.com>
2024-02-15 11:02:58 +0100

    første itkast

---
+++ src/main/java/com/example/data1700oblig1/Data1700Oblig1Application.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Oblig1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data1700Oblig1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/data1700oblig1/Data1700Oblig1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Oblig1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
